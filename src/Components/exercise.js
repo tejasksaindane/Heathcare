470e1fff096e5fa9ad960ac57168762ceeb884d8
tejasksaindane <72094501+tejasksaindane@users.noreply.github.com>
2022-03-04 18:53:20 +0530

    commit

--- src/Components/exercise.js
+++ src/Components/exercise.js
@@ -1,15 +1,32 @@
 import React from "react";
 import "../Styles/Exercise.css";
+import BMI from "../assets/bmi2.png";
+import Dumbbell from "../assets/dumbel.png";
 
 const Exercise = () => {
   return (
     <div className="exercise">
       <div className="card">
         <i></i>
-        <h1>Exercise</h1>
+        <h1>Check Your BMI </h1>
+        <h3>Body Mass Index</h3>
+        <center>
+          <img src={BMI} alt="" width="130vw" />
+        </center>
+        <button>
+          <a href="https://wonderful-galileo-907f1d.netlify.app">Button</a>
+        </button>
       </div>
       <div className="card">
         <i> </i>
+        <h1>Workout Daily </h1>
+        <h3>Join our Fitness Club</h3>
+        <center>
+          <img className="dumbbell" src={Dumbbell} alt="" width="130vw" />
+        </center>
+        <button>
+          <a href="https://wonderful-galileo-907f1d.netlify.app">Button</a>
+        </button>
       </div>
     </div>
   );
